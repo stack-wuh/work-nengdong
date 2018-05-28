1dbc10b465dfc38a9d80140df72a64fb9926eeef
wuhong <wuh131420@foxmail.com>
2018-05-28 14:55:43 +0800

    新增路由地址

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,26 @@ const FirendDetail = resolve => require(['@/components/firendManage/detail'],res
 
 const ActionIndex = resolve => require(['@/components/actionManage/index'],resolve)
 const ActionDetail = resolve => require(['@/components/actionManage/detail'],resolve)
+const ActionItem  = resolve => require(['@/components/actionManage/action'],resolve)
+const ActionList = resolve => require(['@/components/actionManage/list'],resolve)
+const ActionListDetail = resolve => require(['@/components/actionManage/listDetail'],resolve)
 
+const PagesIndex = resolve => require(['@/components/pagesManage/index'],resolve)
+const PagesDetail = resolve => require(['@/components/pagesManage/detail'],resolve)
+const PagesNew = resolve => require(['@/components/pagesManage/new'],resolve)
+const PagesSchool = resolve => require(['@/components/pagesManage/school'],resolve)
+
+const ConcatIndex = resolve => require(['@/components/concatManage/index'],resolve)
+const ConcatDetail = resolve => require(['@/components/concatManage/detail'],resolve)
+
+const DonateIndex = resolve => require(['@/components/donateManage/index'],resolve)
+
+const SettingIndex = resolve => require(['@/components/settingManage/index'],resolve)
+const SettingYear = resolve => require(['@/components/settingManage/child/year'],resolve)
+const SettingConcat = resolve => require(['@/components/settingManage/child/concat'],resolve)
+const SettingAction = resolve => require(['@/components/settingManage/child/action'],resolve)
+const SettingHelp = resolve => require(['@/components/settingManage/child/help'],resolve)
+const SettingFirends = resolve => require(['@/components/settingManage/child/firends'],resolve)
 Vue.use(Router)
 
 export default new Router({
@@ -17,6 +36,7 @@ export default new Router({
     {
       path:'/',
       name:'home',
+      redirect:'/firend',
       component:Home
     },
     {
@@ -38,7 +58,7 @@ export default new Router({
           path:'/firend/detail',
           name:'firendDetail',
           component:FirendDetail
-        }
+        },
       ]
     },
     {
@@ -55,6 +75,116 @@ export default new Router({
           path:'/action/detail',
           name:'actionDetail',
           component:ActionDetail
+        },
+        {
+          path:'/action/action',
+          name:'actionItem',
+          component:ActionItem
+        },
+        {
+          path:'/action/list',
+          name:'actionList',
+          component:ActionList,
+        },
+        {
+          path:'/action/list/detail',
+          name:'actionListDetail',
+          component:ActionListDetail
+        }
+      ]
+    },
+    {
+      path:'/',
+      name:'home',
+      component:Home,
+      children:[
+        {
+          path:'/pages',
+          name:'pages',
+          component:PagesIndex
+        },
+        {
+          path:'/pages/detail',
+          name:'pagesDetail',
+          component:PagesDetail
+        },
+        {
+          path:'/pages/new',
+          name:'pagesNew',
+          component:PagesNew
+        },
+        {
+          path:'/pages/school',
+          name:'pagesSchool',
+          component:PagesSchool
+        }
+      ]
+    },
+    {
+      path:'/',
+      name:'home',
+      component:Home,
+      children:[
+        {
+          path:'/concat',
+          name:'concat',
+          component:ConcatIndex
+        },
+        {
+          path:'/concat/detail',
+          name:'concatDetail',
+          component:ConcatDetail
+        }
+      ]
+    },
+    {
+      path:'/',
+      name:'home',
+      component:Home,
+      children:[
+        {
+          path:'/donate',
+          name:'donate',
+          component:DonateIndex,
+        }
+      ]
+    },
+    {
+      path:'/',
+      name:"home",
+      component:Home,
+      children:[
+        {
+          path:'/setting',
+          name:'setting',
+          component:SettingIndex,
+          children:[
+            {
+              path:'/setting/year',
+              name:'year',
+              component:SettingYear
+            },
+            {
+              path:'/setting/concat',
+              name:'settingConcat',
+              component:SettingConcat
+            },
+            {
+              path:'/setting/action',
+              name:'settingAction',
+              component:SettingAction
+            },
+            {
+              path:'/setting/help',
+              name:'settingHelp',
+              component:SettingHelp
+            },
+            {
+              path:'/setting/firends',
+              name:'settingFirends',
+              component:SettingFirends
+            }
+          ]
         }
       ]
     }
