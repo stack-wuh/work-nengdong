300379425c64d68804dd3c79f32f83de5f998b10
wuhong <wuh131420@foxmail.com>
2018-05-31 19:55:25 +0800

    完成条件搜索

--- src/components/firendManage/index.vue
+++ src/components/firendManage/index.vue
@@ -1,25 +1,22 @@
 <template>
     <section class="wrapper">
-        <search type="1" @handleClickAdd="handleClickAdd" @otherImport="otherImport" @export2excel="export2excel" @propKey="Search" />
+      <search type="1" @handleClickAdd="handleClickAdd" @otherImport="otherImport" @export2excel="export2excel" @propKey="Search" />
       <div class="content">
           <ul class="item-list">
             <li v-for="(item,index) in checkList" :key="index">
-              <span class="item-name">{{item.name}}: </span>
+              <span  class="item-name">{{item.name}}: </span>
               <p>
-                <span v-if="item.type == 1"  v-for="(subItem,subIndex) in item.list" :key="subIndex">{{subItem}}</span>
+                <span :class="{'txt-active' : subItem.isActive}" @click="handleClickChange(subItem,item,index)" v-if="item.type == 1"  v-for="(subItem,subIndex) in item.list" :key="subIndex">{{subItem.name ? subItem.name : subItem[subItem.rename]}}</span>
                 <span style="width:150px;margin-right:20px;" v-if="item.type == 2" v-for="(subItem,subIndex) in item.list" :key="subIndex">
-                  <el-select clearable style="width:150px;height:40px;line-height:40px;" :placeholder="subItem.name" v-model="subItem.value">
-                    <el-option label="1111" value="1111"></el-option>
-                    <el-option label="1112" value="1112"></el-option>
-                    <el-option label="1113" value="1113"></el-option>
-                    <el-option label="1114" value="1114"></el-option>
+                  <el-select @change="handleClickSelect(subIndex,$event,subItem)" clearable style="width:150px;height:40px;line-height:40px;" :placeholder="subItem.name" v-model="subItem.value">
+                    <el-option v-for="(sub,index) in subItem.list" :key="index" :label="sub" :value="sub" ></el-option>
                   </el-select>
                 </span>
               </p>
             </li>
           </ul>
-          <!-- <e-table v-show="false" class="el-table" type="firend" :info="info" /> -->
-          <e-table @handleRowClick="handleRowClick" class="el-table" type="firends" :info="info" />
+          <e-table  @getDelMsg="getDelMsg" class="el-table" type="firend" :info="info" />
+          <e-table v-show="false" @handleRowClick="handleRowClick" class="el-table" type="firends" :info="info" />
           <bottom type="pagination" />
       </div>
     </section>
@@ -29,58 +26,7 @@
 import Search from  '@/components/common/search'
 import ETable from '@/components/common/table'
 import Bottom from '@/components/common/bottom'
-const list = [
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-02',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-08',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-06',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-07',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }
-]
+const list = []
 export default {
   components:{
     Search,
@@ -93,22 +39,26 @@ export default {
         {
           type:1,
           name:'入学年份',
-          list:['2010','2011','2012','2013']
+          prop:'school_age',
+          list:['2010','2011','2012','2013'],
         },
         {
           type:1,
           name:'专业',
-          list:['全部','能动','轮机','油气']
+          prop:'line',
+          list:['全部','能动','轮机','油气'],
         },
         {
           type:1,
           name:'班级',
-          list:['全部','1','2','3']
+          prop:'classes',
+          list:['全部','1','2','3'],
         },
         {
           type:1,
           name:'状态',
-          list:['全部','升学','就业']
+          prop:'state',
+          list:['全部','升学','就业'],
         },
         {
           type:2,
@@ -116,19 +66,27 @@ export default {
           list:[
             {
               name:'单位性质',
-              value:''
+              value:'',
+              prop:'unit_property',
+              list:[]
             },
             {
               name:'所在行业',
               value:'',
+              prop:'unit_way',
+              list:[]
             },
             {
               name:'职位类别',
               value:'',
+              prop:'place_class',
+              list:[]
             },
             {
               name:'层次',
-              value:''
+              value:'',
+              prop:'levels',
+              list:[]
             }
           ]
         }
@@ -136,10 +94,160 @@ export default {
       info:{
         type:'firend',
         list:list
-      }
+      },
+      newList:{},
+      selectList:{}
     }
   },
   methods:{
+    getDelMsg(e){
+      e && this.fetchData()
+    },
+    //获取数据 传给子组件etable
+    fetchData(){
+      let data = Object.assign(this.newList,this.selectList)
+      this.$http('getStudent_Info',data).then(res=>{
+        res.data.map(item=>{
+          item = Object.assign(item,item.employment_archives,item.advance_archives)
+        })
+        this.info.list = res.data
+      })
+    },
+    handleClickSelect(index,$event,item){
+      for(var i=0 ;i<=4;i++){
+        if(i == index){
+          this.selectList[item.prop] = $event
+        }
+      }
+      this.fetchData()
+    },
+    handleClickChange(data,item,index){
+      data.isActive = true
+      item.list.map(subitem=>{
+        if(subitem.id != data.id){
+          subitem.isActive = false
+        }
+      })
+      for(var i=0;i<=4;i++){
+        if(i == index){
+          this.newList[item.prop] = data[data.rename]
+        }
+      }
+      this.fetchData()
+    },
+    //获取入学年份
+    getYears(){
+      this.$http('getYear').then(res=>{
+        this.checkList.map(item=>{
+          if(item.prop == 'school_age'){
+            item.list = res.map(list=>{
+              return list = Object.assign(list,{isActive:false,rename:'age_name'})
+            })
+          }
+        })
+      })
+    },
+    //获取状态
+    getStatus(){
+      this.$http('getStudent_State').then(res=>{
+        this.checkList.map(item=>{
+          if(item.prop == 'state'){
+            item.list = res.map(list=>{
+              return list = Object.assign(list,{isActive:false,rename:'state_name'})
+            })
+          }
+        })
+      })
+    },
+    //获取专业
+    getMajor(){
+      this.$http('getStudent_Info_Line').then(res=>{
+        this.checkList.map(item=>{
+          if(item.prop == 'line'){
+            item.list = res.map(list=>{
+              return list = Object.assign(list,{isActive:false,rename:'line_name'})
+            })
+          }
+        })
+      })
+    },
+    //获取班级
+    getKlasses(){
+      this.$http('getStudent_Info_Class').then(res=>{
+        this.checkList.map(item=>{
+          if(item.prop == 'classes'){
+            item.list = res.map(list=>{
+              return list = Object.assign(list,{isActive:false,rename:'class_name'})
+            })
+          }
+        })
+      })
+    },
+    //获得单位性质
+    getProto(){
+      this.$http('getStudent_Info_Property').then(res=>{
+        this.checkList.map(item=>{
+          if(item.type == 2){
+            item.list.map(list=>{
+              if(list.prop == 'unit_property'){
+                list.list = res.map(sub=>{
+                  return sub.property_name
+                })
+              }
+            })
+          }
+        })
+      })
+    },
+    //获得所在行业
+    getOnWay(){
+      this.$http('getStudent_Info_WayService').then(res=>{
+        this.checkList.map(item=>{
+          if(item.type == 2){
+            item.list.map(list=>{
+              if(list.prop == 'unit_way'){
+                list.list = res.map(sub=>{
+                  return sub.way_name
+                })
+              }
+            })
+          }
+        })
+      })
+    },
+    //获得职位类别
+    getWorkClassify(){
+      this.$http('getStudent_Info_Place_Class').then(res=>{
+        this.checkList.map(item=>{
+          if(item.type == 2){
+            item.list.map(list=>{
+              if(list.prop == 'place_class'){
+                list.list = res.map(sub=>{
+                  return sub.place_class_name
+                })
+              }
+            })
+          }
+        })
+      })
+    },
+    getLevel(){
+      this.$http('getStudent_Info_Level').then(res=>{
+        this.checkList.map(item=>{
+          if(item.type == 2){
+            item.list.map(list=>{
+              if(list.prop == 'levels'){
+                list.list = res.map(sub=>{
+                  return sub.level_name
+                })
+              }
+            })
+          }
+        })
+      })
+    },
+
+
     handleClickAdd(msg){
       console.log('添加')
     },
@@ -155,7 +263,23 @@ export default {
     handleRowClick(e){
       console.log(e)
     },
-  }
+  },
+  created(){
+    this.fetchData()
+    this.getYears()
+    this.getMajor()
+    this.getKlasses()
+    this.getStatus()
+    this.getProto()
+    this.getOnWay()
+    this.getWorkClassify()
+    this.getLevel()
+    setTimeout(()=>{
+     this.checkList.forEach(item=>{
+        item.type == 1 && item.list.unshift({name:'全部',isActive:true})
+      })
+     },1000)
+  },
 };
 </script>
 
@@ -173,7 +297,7 @@ export default {
     // overflow: hidden;
     ul.item-list{
       width: 100%;
-      
+      user-select: none;
       li{
         display: flex;
         align-items: center;
@@ -184,6 +308,9 @@ export default {
         span.item-name{
           width: 70px;
           margin-right:20px;
+        }
+        span:hover{
+          cursor: pointer;
         }
         p{
           span{
