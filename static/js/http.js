e517758287043b543f081e60ee4f03f332b57d78
wuhong <wuh131420@foxmail.com>
2018-06-01 18:14:19 +0800

    httpjs文件修改 删除改变状态的事件

--- static/js/http.js
+++ static/js/http.js
@@ -2,15 +2,18 @@ import qs from 'qs'
 import Axios from 'axios';
 
 const apiPost = (url,data)=>{
-  _g.openGlobalLoading()
+  // _g.openGlobalLoading()
+  store.commit('changeLoading',{state:true})
   url = rootPath + url
   data = qs.stringify(data)
   return new Promise((resolve,reject)=>{
     Axios.post(url,data).then(res=>{
-      _g.closeGlobalLoading()
+      // _g.closeGlobalLoading()
+      store.commit('changeLoading',{state:false})
       resolve(res.data)
     }).catch(res=>{
-      _g.closeGlobalLoading()
+      // _g.closeGlobalLoading()
+      store.commit('changeLoading',{state:false})
       resolve(res)
       $bus.$message({
         message:'请求超时,请检查网络',
