9fb24abe026ad52015517872f88336581c92ba21
wuhong <wuh131420@foxmail.com>
2018-06-07 18:55:44 +0800

    2018-06-07-vuex文件新增

--- src/vuex/modules/form.js
+++ src/vuex/modules/form.js
@@ -40,6 +40,7 @@ const state = {
       checkword:''
     }
   },
+
   //设置 -- 添加年份
   addNewYear:{
     info:[
@@ -68,6 +69,7 @@ const state = {
       age_or:1,
     }
   },
+
   //活动 -- 添加反馈
   addFeedback:{
     info:[
@@ -100,8 +102,7 @@ const state = {
         name:'填写反馈',
         value:'',
         prop:'feedback',
-        isInput:true,
-        inputType:'textArea',
+        isInput:false,
         isTextarea:true,
         row:2,
         rules:[
@@ -117,6 +118,7 @@ const state = {
       feedback:''
     }
   },
+
   //设置 -- 添加互助类型
   addHelp:{
     info:[
@@ -166,9 +168,8 @@ const state = {
       proclaim:''
     }
   },
-
-  //编辑学院详情
-  editCollege:{
+  //添加学院
+  addPagesSchool:{
     info:[
       {
         name:'学院名称',
@@ -183,7 +184,7 @@ const state = {
         ]
       },
       {
-        name:'电话',
+        name:'学院电话',
         prop:'phone',
         isInput:true,
         rules:[
@@ -200,6 +201,75 @@ const state = {
       phone:'',
       id:''
     }
+  },
+  //编辑学院详情
+  editCollege:{
+    info:[
+      {
+        name:'学院名称',
+        prop:'school_name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请输入学院名称',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'学院电话',
+        prop:'phone',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请输入学院电话',
+            trigger:'blur'
+          }
+        ]
+      }
+    ],
+    validForm:{
+      school_name:'',
+      phone:'',
+      id:''
+    }
+  },
+
+  //添加学校
+  addPagesCollege:{
+    info:[
+      {
+        name:'学校名称',
+        isInput:true,
+        prop:'name',
+        rules:[
+          {
+            required:true,
+            message:'请输入学校名称',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'学校电话',
+        isInput:true,
+        prop:'phone',
+        rules:[
+          {
+            required:true,
+            message:'请输入学校电话',
+            trigger:'blur'
+          }
+        ]
+      }
+    ],
+    validForm:{
+      name:'',
+      phone:'',
+      id:''
+    } 
   },
   //编辑学校详情:
   editSchool:{
@@ -372,6 +442,7 @@ const state = {
       color:''
     }
   },
+  //设置 -- 活动类型--编辑活动类型
   editActionType:{
     info:[
       {
@@ -427,6 +498,459 @@ const state = {
       pupil:'',
       id:'',
       color:''
+    }
+  },
+  //设置 -- 添加专业设置
+  addSettingMajor:{
+    info:[
+      {
+        name:'专业名称',
+        prop:'line_name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写专业名称',
+            trigger:'blur'
+          }
+        ]
+      }
+    ],
+    validForm:{
+      line_name:'',
+      id:''
+    }
+  },
+  //设置 -- 编辑专业设置
+  editSettingMajor:{
+    info:[
+      {
+        name:'专业名称',
+        prop:'line_name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写专业名称',
+            trigger:'blur'
+          }
+        ]
+      }
+    ],
+    validForm:{
+      line_name:'',
+      id:''
+    }
+  },
+  //设置 -- 添加班级设置
+  addSettingKlass:{
+    info:[
+      {
+        name:"班级名称",
+        prop:'class_name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写班级名称',
+            trigger:'blur'
+          }
+        ]
+      }
+    ],
+    validForm:{
+      class_name:''
+    }
+  },
+  //设置--编辑班级设置
+  editSettingKlass:{
+    info:[
+      {
+        name:"班级名称",
+        prop:'class_name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写班级名称',
+            trigger:'blur'
+          }
+        ]
+      }
+    ],
+    validForm:{
+      class_name:'',
+      id:''
+    } 
+  },
+  //设置 -- 添加官方组织
+  addSettingOfficial:{
+    info:[
+      {
+        name:'组织名称',
+        prop:'official_name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写官方组织名',
+            trigger:'blur'
+          }
+        ]
+      }
+    ],
+    validForm:{
+      official_name:''
+    }
+  },
+  //设置 -- 编辑官方组织
+  editSettingOfficial:{
+    info:[
+      {
+        name:'组织名称',
+        prop:'official_name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写官方组织名',
+            trigger:'blur'
+          }
+        ]
+      }
+    ],
+    validForm:{
+      official_name:''
+    }
+  },
+
+  //设置--添加联系学院
+  addConcatSchool:{
+    info:[
+      {
+        name:'问题类型',
+        isSelect:true,
+        prop:'question_type',
+        list:[
+          {
+            name:'忘记密码',
+            value:'忘记密码'
+          },
+          {
+            name:'校友身份认证失败',
+            value:'校友身份认证失败'
+          },
+          {
+            name:'其他',
+            value:'其他'
+          }
+        ],
+        rules:[
+          {
+            required:true,
+            message:'请选择问题类型',
+            trigger:'change'
+          }
+        ]
+      },
+      {
+        name:'姓名',
+        prop:'name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写姓名',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'详细说明',
+        prop:'illustrate',
+        isTextarea:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写详细说明',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'手机号',
+        prop:'phone',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写手机号',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'邮箱',
+        prop:'email',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写邮箱地址',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'入学年份',
+        prop:'year',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写入学年份',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'学号',
+        prop:'number',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写学号',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'身份证号',
+        prop:'no',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写身份证号',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'专业班级',
+        prop:'classes',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写专业班级',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'辅导员',
+        prop:'counsellor',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写辅导员',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'图片',
+        prop:'file',
+        isUpload:true,
+        rules:[
+          {
+            required:true,
+            message:'请上传图片',
+            trigger:'change'
+          }
+        ]
+      },                                
+    ],
+    validForm:{
+      question_type:'',
+      illustrate:'',
+      phone:'',
+      email:'',
+      year:'',
+      number:'',
+      no:'',
+      classes:'',
+      counsellor:'',
+      name:'',
+      file:''
+    }
+  },
+  //设置--编辑联系学院
+  editSettingConcat:{
+    info:[
+      {
+        name:'问题类型',
+        isSelect:true,
+        prop:'question_type',
+        list:[
+          {
+            name:'忘记密码',
+            value:'忘记密码'
+          },
+          {
+            name:'校友身份认证失败',
+            value:'校友身份认证失败'
+          },
+          {
+            name:'其他',
+            value:'其他'
+          }
+        ],
+        rules:[
+          {
+            required:true,
+            message:'请选择问题类型',
+            trigger:'change'
+          }
+        ]
+      },
+      {
+        name:'姓名',
+        prop:'name',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写姓名',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'详细说明',
+        prop:'illustrate',
+        isTextarea:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写详细说明',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'手机号',
+        prop:'phone',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写手机号',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'邮箱',
+        prop:'email',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写邮箱地址',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'入学年份',
+        prop:'year',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写入学年份',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'学号',
+        prop:'number',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写学号',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'身份证号',
+        prop:'no',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写身份证号',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'专业班级',
+        prop:'classes',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写专业班级',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'辅导员',
+        prop:'counsellor',
+        isInput:true,
+        rules:[
+          {
+            required:true,
+            message:'请填写辅导员',
+            trigger:'blur'
+          }
+        ]
+      },
+      {
+        name:'图片',
+        prop:'file',
+        isUpload:true,
+        rules:[
+          {
+            required:true,
+            message:'请上传图片',
+            trigger:'change'
+          }
+        ]
+      },                                
+    ],
+    validForm:{
+      question_type:'',
+      illustrate:'',
+      phone:'',
+      email:'',
+      year:'',
+      number:'',
+      no:'',
+      classes:'',
+      counsellor:'',
+      name:'',
+      file:'',
+      id:''
     }
   }
 }
